2ef31e8fb5b81bfa80421e4a7cdf54a70d386882
Mdsoln <muddyfakih98@gmail.com>
2024-11-26 10:48:21 +0300

    resolving fatal

--- models.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-try:
-    with open("soil_health_model_v2.pkl", "rb") as f:
-        model = pickle.load(f)
-    print("Model loaded successfully!")
-except Exception as e:
-    print(f"Error loading model: {e}")
